f2c3d9530580b9e403880d286c422de90fb374ff
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-06 23:28:12 +0530

    change in model valodation

--- db/migrate/20250305195313_cerate_articles.rb
+++ db/migrate/20250305195313_cerate_articles.rb
@@ -2,7 +2,7 @@ class CerateArticles < ActiveRecord::Migration[7.1]
   def change # The change method is the primary method for writing migrations. It works for the majority of cases, where ActiveRecord knows how to reverse the migration automatically.
     create_table :articles do |t|
       t.string :title
-      t.text :description
+      t.text :description # The t.text data type is for longer pieces of textual data, such as a blog post body.
     end
   end
 end
