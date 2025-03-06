8eca328e6460cda47f4dbbd947a8601be6b49563
pravin.hudekar@definelabs.com <pravin.hudekar@definelabs.com>
2025-03-06 23:35:41 +0530

    git new branch created

---
+++ my_rails/gettingInput.rb
@@ -0,0 +1,7 @@
+# puts "Enter your Name:"
+# input = gets.chomp 
+# puts input
+
+puts "Enter your Name:"
+input = gets.chomp  
+puts "You entered: #{input}" 
---
+++ my_rails_project/string.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# sentence = 'My name is Pravin'
+# first_name = 'Pravin'
+# last_name = 'Hudekar'
+# puts(first_name + '  ' + last_name)
+
+# puts "My first name id #{first_name} and my last name is #{last_name}"
+# puts last_name.reverse
+# puts first_name.capitalize
+# String methods
+# # String Concatenation
+# name = 'Pravin'
+# greeting = 'Hello'
+
+# puts greeting = "Hello1" + name
+# puts greeting = "Hello2 #{name}"
+# puts "Hello3" <<name
+
+# #Changing case 
+# text = "pravin hudekar"
+# puts text.upcase
+# puts text.downcase
+# puts text.capitalize
+# puts text.swapcase
+
+# #Checking String length
+#  str ="pravin hudekar"
+#  puts str.length
+#  puts str.size
+
+# #Checking if String Contains a SubString 
+# puts "hello".include?("ell")
+# puts "hello".start_with?("he")
+# puts "hello".end_with?("lo")
+
+# #String indexing
+# text ="Ruby"
+
+# puts text[0]
+# puts text[-1]
+# puts text[1,2]
+# puts text[0..2]
+
+# #Removing Whitespace
+# str ="  Ruby  "
+# puts str.strip
+# puts str.lstrip
+# puts str.rstrip
+
+ #Replacing Charecter
+ text = "I Love Ruby"
+ puts text.sub("Ruby", "Java")
+ puts text.gsub("","_")
+
+ 
